5443a5a91f22401f948cde1f4cad26b81b618b48
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2022-10-09 23:43:29 -0400

    Changed Header

--- PassGenV1.1.c
+++ PassGenV1.1.c
@@ -2,9 +2,5 @@
 // Author as Dielle De Noon
 // v1.1
 // PassGen Header
-printif(" _____                         _____                ")
-printif("|  __ \                       / ____|               ")
-printif("| |__) |   __ _   ___   ___  | |  __    ___   _ __  ")
-printif("|  ___/   / _` | / __| / __| | | |_ |  / _ \ | '_ \ ")
-printif("| |      | (_| | \__ \ \__ \ | |__| | |  __/ | | | |")
-printif("|_|       \__,_| |___/ |___/  \_____|  \___| |_| |_|")
+printif("Welcome to PassGen")
+
